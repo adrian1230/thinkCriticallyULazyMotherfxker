91bb19d69e0dbbd824c8cfebe0270af62e771d46
adrian1230 <adrian1145d98@gmail.com>
2021-01-03 21:52:13 +0800

    adrian

---
+++ android/app/src/main/java/com/be_a_critical_thinker_and_suspect_of_everything_til_u_can_explain_it_urself/MainActivity.java
@@ -0,0 +1,15 @@
+package com.be_a_critical_thinker_and_suspect_of_everything_til_u_can_explain_it_urself;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "be_a_critical_thinker_and_suspect_of_everything_til_u_can_explain_it_urself";
+  }
+}
