0c727e8c782e852c5a53f726483045a7adf60ce2
adrian1230 <adrian1145d98@gmail.com>
2021-01-04 21:24:53 +0800

    adrian

--- App.js
+++ App.js
@@ -8,11 +8,7 @@
 
 import React from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
-  View,
-  Text,
   StatusBar,
 } from 'react-native';
 
@@ -28,31 +24,6 @@ const App = () => {
 };
 
 const styles = StyleSheet.create({
-  scrollView: {},
-  body: {
-    height: '100%',
-    width: '100%',
-    backgroundColor: '#edf2ef',
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  sectionContainer: {
-    marginTop: 20,
-    paddingHorizontal: 28,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: 'black',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: 'black',
-  },
 });
 
 export default App;
