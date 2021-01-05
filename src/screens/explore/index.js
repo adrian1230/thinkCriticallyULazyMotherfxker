b211ecdc483a1144fe91874df8bbf4d6bdf24487
adrian1230 <adrian1145d98@gmail.com>
2021-01-05 16:58:32 +0800

    adrian

--- src/screens/explore/index.js
+++ src/screens/explore/index.js
@@ -1,20 +1,31 @@
 import * as React from 'react';
-import {View,Button,SafeAreaView,Text} from 'react-native';
+import {View,Button,StyleSheet,SafeAreaView,Text} from 'react-native';
 
 const Explore = ({ navigation }) => {
     return (
         <SafeAreaView>
             <View>
                 <Text>Explore</Text>
-                {/* <Button
-                    title="Cart"
-                    onPress={
-                        () => navigation.navigate('Cart')
-                    } 
-                /> */}
+                <View style={styles.btn}>
+                    <Button
+                        title="Profile"
+                        onPress={
+                            () => navigation.navigate('Profile')
+                        } 
+                    />
+                </View>
             </View>
         </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    btn: {
+        width: '50%',
+        alignSelf: 'center',
+        marginTop: 15,
+        borderRadius: 25
+    }
+})
 
 export default Explore;
