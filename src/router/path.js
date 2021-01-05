d9bfb439cce85cc3d86ebaccde3c01a6a9afdcf3
adrian1230 <adrian1145d98@gmail.com>
2021-01-05 15:27:26 +0800

    adrian

--- src/router/path.js
+++ src/router/path.js
@@ -11,111 +11,121 @@ import Explore from '../screens/explore/index';
 import Profile from '../screens/profile/index';
 import {icons} from '../../constants/index';
 import 'react-native-gesture-handler';
-import { withNavigation,DrawerActions } from "react-navigation";
+import { DrawerActions } from "react-navigation";
+import { NativeScreenContainer } from 'react-native-screens';
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 
-class RollerDraw extends React.PureComponent {
-    toggleDrawer = () => {
-        this.props.navigation.dispatch(DrawerActions.toggleDrawer())
+const RollerDraw = (props) => {
+    const toggle = () => {
+        props.navigationProps.toggle();
     };
 
-    render() {
-        return (
-            <View style={{ flexDirection: 'row' }}>
-                <TouchableOpacity
-                    onPress={()=> this.toggleDrawer()} 
-                    style={{height:'100%'}}
-                >
-                    <Image 
-                        source={icons.orezaar} 
-                        resizeMode="cover"
-                        style={{
-                            width: 50,
-                            height: 50,
-                            marginTop: 3,
-                            marginLeft: 5
-                        }}
-                    />
-                </TouchableOpacity>
-            </View>
-        );
-    }
-}
-
-function ExploreScreenStack({navigation}) {
     return (
-        <Stack.Navigator initialRouteName="Explore">
-            <Stack.Screen
-                name="Explore"
-                component={Explore}
-                options={{
-                    title: 'Explore',
-                    headerLeft: () => 
-                        <RollerDraw
-                            navigationProps={navigation}
-                        />
-                    ,
-                    headerStyle: {
-                        backgroundColor: "grey"
-                    },
-                    headerTintColor: 'orange',
-                    headerTitleStyle: {
-                        fontWeight: '600'
-                    }
-                }} 
-            />
-        </Stack.Navigator>
+        <View style={{ flexDirection: 'row' }}>
+            <TouchableOpacity
+                onPress={()=> toggle()} 
+                style={{height:'100%'}}
+            >
+                <Image 
+                    source={icons.orezaar} 
+                    resizeMode="cover"
+                    style={{
+                        width: 50,
+                        height: 50,
+                        marginTop: 3,
+                        marginLeft: 5
+                    }}
+                />
+            </TouchableOpacity>
+        </View>
     );
 }
 
-function ProfileScreenStack({navigation}) {
-    return (
-        <Stack.Navigator initialRouteName="Profile">
-            <Stack.Screen
-                name="Profile"
-                component={Profile}
-                options={{
-                    title: 'Profile',
-                    headerLeft: () => 
-                        <RollerDraw
-                            navigationProps={navigation}
-                        />
-                    ,
-                    headerStyle: {
-                        backgroundColor: "grey"
-                    },
-                    headerTintColor: 'orange',
-                    headerTitleStyle: {
-                        fontWeight: '600'
-                    }
-                }} 
-            />
-        </Stack.Navigator>
-    );
-}
+// function ExploreScreenStack({navigation}) {
+//     return (
+//         <Stack.Navigator initialRouteName="Explore">
+//             <Stack.Screen
+//                 name="Explore"
+//                 component={Explore}
+//                 options={{
+//                     title: 'Explore',
+//                     headerLeft: () => 
+//                         <RollerDraw
+//                             navigationProps={navigation}
+//                         />
+//                     ,
+//                     headerStyle: {
+//                         backgroundColor: "grey"
+//                     },
+//                     headerTintColor: 'orange',
+//                     headerTitleStyle: {
+//                         fontWeight: '600'
+//                     }
+//                 }} 
+//             />
+//         </Stack.Navigator>
+//     );
+// }
+
+// function ProfileScreenStack({navigation}) {
+//     return (
+//         <Stack.Navigator initialRouteName="Profile">
+//             <Stack.Screen
+//                 name="Profile"
+//                 component={Profile}
+//                 options={{
+//                     title: 'Profile',
+//                     headerLeft: () => 
+//                         <RollerDraw
+//                             navigationProps={navigation}
+//                         />
+//                     ,
+//                     headerStyle: {
+//                         backgroundColor: "grey"
+//                     },
+//                     headerTintColor: 'orange',
+//                     headerTitleStyle: {
+//                         fontWeight: '600'
+//                     }
+//                 }} 
+//             />
+//         </Stack.Navigator>
+//     );
+// }
+
+// function Path() {
+//     return (
+//         <NavigationContainer>
+//             <Drawer.Navigator
+//                 drawerContentOptions={{
+//                     activeTintColor: 'red',
+//                     itemStyle: {marginVertical:7}
+//                 }}
+//             >
+//                 <Drawer.Screen
+//                     name="Explore"
+//                     options={{drawerLabel: 'Explorer'}}
+//                     component={ExploreScreenStack} 
+//                 />
+//                 <Drawer.Screen
+//                     name="Profile"
+//                     options={{drawerLabel: 'Profile'}}
+//                     component={ProfileScreenStack} 
+//                 />
+//             </Drawer.Navigator>
+//         </NavigationContainer>
+//     );
+// }
 
 function Path() {
-    return (
+    return(
         <NavigationContainer>
-            <Drawer.Navigator
-                drawerContentOptions={{
-                    activeTintColor: 'red',
-                    itemStyle: {marginVertical:7}
-                }}
-            >
-                <Drawer.Screen
-                    name="Explore"
-                    options={{drawerLabel: 'Explorer'}}
-                    component={ExploreScreenStack} 
-                />
-                <Drawer.Screen
-                    name="Profile"
-                    options={{drawerLabel: 'Profile'}}
-                    component={ProfileScreenStack} 
-                />
-            </Drawer.Navigator>
+            <Drawer.Navigator initialRouteName="Explore">
+                <Drawer.Screen name="Explore" component={Explore} />
+                <Drawer.Screen name="Profile" component={Profile} />
+            </Drawer.Navigator> 
         </NavigationContainer>
     );
 }
