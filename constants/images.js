49a23e474c513e64bd3e4320a80cd20c504360a0
adrian1230 <adrian1145d98@gmail.com>
2021-01-06 03:22:01 +0800

    adrian

---
+++ constants/images.js
@@ -0,0 +1,27 @@
+export const _1 = require('../assets/images/1.png');
+export const _2 = require('../assets/images/2.png');
+export const _3 = require('../assets/images/3.png');
+export const _4 = require('../assets/images/4.png');
+export const _5 = require('../assets/images/5.png');
+export const _6 = require('../assets/images/6.png');
+export const _7 = require('../assets/images/7.png');
+export const _8 = require('../assets/images/8.png');
+export const _9 = require('../assets/images/9.png');
+export const _10 = require('../assets/images/10.png');
+export const _11 = require('../assets/images/11.png');
+export const _12 = require('../assets/images/12.png');
+export const _13 = require('../assets/images/13.png');
+export const _14 = require('../assets/images/14.png');
+export const _15 = require('../assets/images/15.png');
+export const _16 = require('../assets/images/16.png');
+export const _17 = require('../assets/images/17.png');
+export const _18 = require('../assets/images/18.png');
+export const _19 = require('../assets/images/19.png');
+export const _20 = require('../assets/images/20.png');
+export const _21 = require('../assets/images/21.png');
+
+export default {
+    _1,_2,_3,_4,_5,_6,_7,_8,_9,_10,
+    _11,_12,_13,_14,_15,_16,_17,_18,
+    _19,_20,_21
+}
