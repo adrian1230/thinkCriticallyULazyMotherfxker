ddddbb44a9797b4023ddb73b72cfeab6dc5fcf66
adrian1230 <adrian1145d98@gmail.com>
2021-01-07 00:30:04 +0800

    adrian

--- src/screens/explore/index.js
+++ src/screens/explore/index.js
@@ -32,133 +32,133 @@ const Explore = ({ navigation }) => {
         },
         {
             "id": 2,
-            "name": "Backpack",
+            "name": "Switch",
             "img": images._3,
             "height": Image.resolveAssetSource(images._3).height + 100,
             "width": Image.resolveAssetSource(images._3).width
         },
         {
             "id": 3,
-            "name": "Backpack",
+            "name": "Vibrator",
             "img": images._4,
             "height": Image.resolveAssetSource(images._4).height + 100,
             "width": Image.resolveAssetSource(images._4).width
         },
         {
             "id": 4,
-            "name": "Backpack",
+            "name": "BlueApron",
             "img": images._5,
             "height": Image.resolveAssetSource(images._5).height + 100,
             "width": Image.resolveAssetSource(images._5).width
         },
         {
             "id": 5,
-            "name": "Backpack",
+            "name": "HotPot Package",
             "img": images._6,
             "height": Image.resolveAssetSource(images._6).height + 100,
             "width": Image.resolveAssetSource(images._6).width
         },
         {
             "id": 6,
-            "name": "Backpack",
+            "name": "HotPot Griller",
             "img": images._7,
             "height": Image.resolveAssetSource(images._7).height + 100,
             "width": Image.resolveAssetSource(images._7).width
         },
         {
             "id": 7,
-            "name": "Backpack",
+            "name": "Durex",
             "img": images._8,
             "height": Image.resolveAssetSource(images._8).height + 100,
             "width": Image.resolveAssetSource(images._8).width
         },
         {
             "id": 8,
-            "name": "Backpack",
+            "name": "HarryPotter CD",
             "img": images._9,
             "height": Image.resolveAssetSource(images._9).height + 100,
             "width": Image.resolveAssetSource(images._9).width
         },
         {
             "id": 9,
-            "name": "Backpack",
+            "name": "Scalett Recording",
             "img": images._10,
             "height": Image.resolveAssetSource(images._10).height + 100,
             "width": Image.resolveAssetSource(images._10).width
         },
         {
             "id": 10,
-            "name": "Backpack",
+            "name": "VR Set",
             "img": images._11,
             "height": Image.resolveAssetSource(images._11).height + 100,
             "width": Image.resolveAssetSource(images._11).width
         },
         {
             "id": 11,
-            "name": "Backpack",
+            "name": "Perfume 1o3",
             "img": images._12,
             "height": Image.resolveAssetSource(images._12).height + 100,
             "width": Image.resolveAssetSource(images._12).width
         },
         {
             "id": 12,
-            "name": "Backpack",
+            "name": "Pot",
             "img": images._13,
             "height": Image.resolveAssetSource(images._13).height + 100,
             "width": Image.resolveAssetSource(images._13).width
         },
         {
             "id": 13,
-            "name": "Backpack",
+            "name": "Shaving Gel",
             "img": images._14,
             "height": Image.resolveAssetSource(images._14).height + 100,
             "width": Image.resolveAssetSource(images._14).width
         },
         {
             "id": 14,
-            "name": "Backpack",
+            "name": "External Monitor",
             "img": images._15,
             "height": Image.resolveAssetSource(images._15).height + 100,
             "width": Image.resolveAssetSource(images._15).width
         },
         {
             "id": 15,
-            "name": "Backpack",
+            "name": "Milk",
             "img": images._16,
             "height": Image.resolveAssetSource(images._16).height + 100,
             "width": Image.resolveAssetSource(images._16).width
         },
         {
             "id": 16,
-            "name": "Backpack",
+            "name": "Hiking Stick",
             "img": images._17,
             "height": Image.resolveAssetSource(images._17).height + 100,
             "width": Image.resolveAssetSource(images._17).width
         },
         {
             "id": 17,
-            "name": "Backpack",
+            "name": "Booty Band",
             "img": images._18,
             "height": Image.resolveAssetSource(images._18).height + 100,
             "width": Image.resolveAssetSource(images._18).width
         },
         {
             "id": 18,
-            "name": "Backpack",
+            "name": "Adjustable Dumbbell",
             "img": images._19,
             "height": Image.resolveAssetSource(images._19).height + 100,
             "width": Image.resolveAssetSource(images._19).width
         },
         {
             "id": 19,
-            "name": "Backpack",
+            "name": "Drum Set",
             "img": images._20,
             "height": Image.resolveAssetSource(images._20).height + 100,
             "width": Image.resolveAssetSource(images._20).width
         },
         {
             "id": 20,
-            "name": "Backpack",
+            "name": "Masked Hoodie",
             "img": images._21,
             "height": Image.resolveAssetSource(images._21).height + 100,
             "width": Image.resolveAssetSource(images._21).width
