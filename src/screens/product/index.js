b50e22a1901bfe2b2af135b08c8dd62506382bd7
adrian1230 <adrian1145d98@gmail.com>
2021-01-07 23:49:48 +0800

    adrian

--- src/screens/product/index.js
+++ src/screens/product/index.js
@@ -3,7 +3,8 @@ import {
     View,
     SafeAreaView,
     Text,
-    ScrollView
+    ScrollView,
+    Image
 } from 'react-native';
 import {stats} from '../../../constants/index';
 
@@ -11,8 +12,27 @@ const Product = ({ route }) => {
     return (
         <SafeAreaView>
             <ScrollView>
-                <Text>Product</Text>
-                <Text>{route.params.itemId}</Text>
+                {/* <Text>{route.params.itemId}</Text> */}
+                <View style={{
+                    marginTop: 15,
+                }}>
+                    <Image
+                        style={{
+                            alignSelf: 'center',
+                        }}
+                        source={
+                            stats.data_[
+                                route.params.itemId
+                            ].img
+                        } 
+                    />
+                    <Text style={{marginTop:10,fontSize:17,fontWeight:'700',alignSelf:'center'}}>
+                        {stats.data_[route.params.itemId].name}
+                    </Text>
+                    <Text style={{marginTop:2,fontSize:14,alignSelf:'center'}}>
+                        {stats.data_[route.params.itemId].description}
+                    </Text>
+                </View>
             </ScrollView>
         </SafeAreaView>
     );
