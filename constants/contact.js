e3bec6b03229e25fe44fa11c9bd831d37bf39046
adrian1230 <adrian1145d98@gmail.com>
2021-01-07 20:16:57 +0800

    adrian

--- constants/contact.js
+++ constants/contact.js
@@ -1,41 +1,51 @@
 export const ppl = [
     {
+        'id': 0,
         'name': 'abc',
         'groupchat': true,
     },
     {
+        'id': 1,
         'name': 'adrian',
         'groupchat': false,
     },
     {
+        'id': 2,
         'name': 'abhasTam',
         'groupchat': true,
     },
     {
+        'id': 3,
         'name': 'Vivian',
         'groupchat': true,
     },
     {
+        'id': 4,
         'name': 'Bus',
         'groupchat': true,
     },
     {
+        'id': 5,
         'name': 'John',
         'groupchat': false,
     },
     {
+        'id': 6,
         'name': 'Calvin',
         'groupchat': false,
     },
     {
+        'id': 7,
         'name': 'Devid',
         'groupchat': false,
     },
     {
+        'id': 8,
         'name': 'Kol',
         'groupchat': true,
     },
     {
+        'id': 9,
         'name': 'Bitch',
         'groupchat': false,
     },
