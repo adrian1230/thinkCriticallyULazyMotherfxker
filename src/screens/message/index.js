dc1952c8c25ec151c9b32e26a7c5d51b6f9598f2
adrian1230 <adrian1145d98@gmail.com>
2021-01-07 18:53:55 +0800

    adrian

--- src/screens/message/index.js
+++ src/screens/message/index.js
@@ -1,14 +1,37 @@
 import * as React from 'react';
-import {View,SafeAreaView,Text} from 'react-native';
+import {
+    View,
+    SafeAreaView,
+    TextInput
+} from 'react-native';
 
 const Chat = ({ navigation }) => {
     return (
         <SafeAreaView>
-            <View>
-                <Text>Chat</Text>
+            <View 
+                style={{
+                    marginHorizontal: 25,
+                    marginTop: 10
+                }}
+            >
+                <TextInput
+                    style={{
+                        backgroundColor:'#c4c3c2',
+                        width: '70%',
+                        alignSelf: 'center',
+                        borderRadius: 15,
+                        marginTop: 15,
+                        paddingLeft: 20,
+                        color: 'white',
+                        fontWeight: '600',
+                        fontSize: 15
+                    }}
+                    placeholder="Search Chat" 
+                />
             </View>
         </SafeAreaView>
     );
 }
 
 export default Chat;
+
